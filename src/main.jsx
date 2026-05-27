91bb5c1079036a14c31d02c8e8f69c5f0c1c5161
samucarsilva <samucarsilva007@gmail.com>
2026-05-27 09:00:22 -0300

    chore: adiciona scripts do bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import App from "./App.jsx";
 
