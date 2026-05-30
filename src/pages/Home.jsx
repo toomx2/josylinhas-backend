44161b05d96bc0361bb3f7d275356f8f9f88d27d
samucarsilva <samucarsilva007@gmail.com>
2026-05-30 14:22:48 -0300

    refactor: remove container redundante da home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,29 +4,27 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-        <div className="josylinhas-main">
-            <div className="josylinhas-hero">
-                <div className="d-flex flex-column justify-content-center flex-wrap vh-100">
+        <div className="josylinhas-hero">
+            <div className="d-flex flex-column justify-content-center flex-wrap vh-100">
 
-                    <div className="container hero-background shadow-lg">
+                <div className="container hero-background shadow-lg">
 
-                        <h1 className="display-4 font-semibold text-center">
-                            Boas-Vindas à Josylinhas!
-                        </h1>
+                    <h1 className="display-4 font-semibold text-center">
+                        Boas-Vindas à Josylinhas!
+                    </h1>
 
-                        <p className="lead text-center">
-                            Aqui, cada costura conta uma história e cada peça é feita com amor, criatividade e consciência
-                        </p>
+                    <p className="lead text-center">
+                        Aqui, cada costura conta uma história e cada peça é feita com amor, criatividade e consciência
+                    </p>
 
-                        <div className="d-grid justify-content-center mt-5">
-                            <Link className="josylinhas-btn btn-secondary" to="/produtos">
-                                Nossos Produtos
-                            </Link>
-                        </div>
-
+                    <div className="d-grid justify-content-center mt-5">
+                        <Link className="josylinhas-btn btn-secondary" to="/produtos">
+                            Nossos Produtos
+                        </Link>
                     </div>
 
                 </div>
+
             </div>
         </div>
     );
