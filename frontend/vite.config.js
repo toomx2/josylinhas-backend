a0ca4ea6ff82f7db730f5c5bb14da5a29486b683
samucarsilva <samucarsilva007@gmail.com>
2026-07-06 09:10:13 -0300

    fix: corrige caminho de busca do arquivo .env no vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,23 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
 
-    const env = loadEnv(mode, process.cwd() + "/../");
-    const port = env.VITE_PORT || 5000;
+    const env = loadEnv(mode, path.resolve(__dirname));
+    const apiTarget = env.VITE_API_URL || `http://localhost:${env.VITE_PORT || 5000}`;
 
     return {
         plugins: [react()],
         server: {
             proxy: {
                 "/api": {
-                    target: `http://localhost:${port}`,
+                    target: apiTarget,
                     changeOrigin: true,
                     rewrite: (path) => path.replace(/^\/api/, ""),
                 },
             },
         },
     }
+
 });
